c9f39234879dd0d58fdc01b5c02b6c4cfa26ef31
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-12-23 14:57:43 +0530

    auth layout leftside done

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex items">
+      <h1 className="text-3xl text-brand">StoreIt</h1>
+      <p>the only storage solution you need</p>
+    </div>
+  );
+};
+
+export default Home;
