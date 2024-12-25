5a6a0ede315288a7a5413bcb84206bf54be464c3
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-12-25 22:56:30 +0530

    made getFileType util func

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,10 +1,28 @@
 "user client";
 
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
+import Image from "next/image";
 import { useDropzone } from "react-dropzone";
 
+// UI imports
+import { Button } from "./ui/button";
+
+// utils imports
+import { cn } from "@/lib/utils";
+
 // current component ⚛️
-const FileUploader = () => {
+const FileUploader = ({
+  ownerId,
+  accountId,
+  classNames,
+}: {
+  ownerId: string;
+  accountId: string;
+  classNames?: string;
+}) => {
+  // states
+  const [files, setFiles] = useState<File[]>([]);
+
   // hook🪝
   const onDrop = useCallback((acceptedFiles) => {
     // Do something with the files
@@ -12,8 +30,27 @@ const FileUploader = () => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
+    <div {...getRootProps()} className="cursor-pointer">
       <input {...getInputProps()} />
+
+      {/* upload button */}
+      <Button type="button" className={cn("uploader-button", classNames)}>
+        <Image
+          src="/assets/icons/upload.svg"
+          alt="upload"
+          height={24}
+          width={24}
+        />
+        <p>Upload</p>
+      </Button>
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uplaoding</h4>
+          {files.map((file) => {
+            const { type, extension } = getFileType(file.name);
+          })}
+        </ul>
+      )}
       {isDragActive
         ? <p>Drop the files here ...</p>
         : <p>Drag 'n' drop some files here, or click to select files</p>}
--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,33 @@ export function cn(...inputs: ClassValue[]) {
 export const parseStringify = (data: unknown) => {
   return JSON.parse(JSON.stringify(data));
 };
+
+export const getFileType = (fileName: string) => {
+  const extension = fileName.split(".").pop()?.toLowerCase() || "";
+  let type = "unknown";
+
+  const imageExtensions = ["jpg", "jpeg", "png", "gif", "bmp", "webp"];
+  const videoExtensions = ["mp4", "mkv", "mov", "avi", "wmv"];
+  const audioExtensions = ["mp3", "wav", "aac", "flac", "ogg"];
+  const documentExtensions = [
+    "pdf",
+    "doc",
+    "docx",
+    "xls",
+    "xlsx",
+    "ppt",
+    "pptx",
+  ];
+
+  if (imageExtensions.includes(extension)) {
+    type = "image";
+  } else if (videoExtensions.includes(extension)) {
+    type = "video";
+  } else if (audioExtensions.includes(extension)) {
+    type = "audio";
+  } else if (documentExtensions.includes(extension)) {
+    type = "document";
+  }
+
+  return { type, extension };
+};
