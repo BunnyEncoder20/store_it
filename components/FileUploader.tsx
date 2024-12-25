78bf3c746886d23d5e9985dd759a41bd6b6d13b6
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-12-25 22:34:54 +0530

    working on fileuploader

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"user client";
 
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+// current component ⚛️
 const FileUploader = () => {
-  return <div>FileUploader</div>;
+  // hook🪝
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive
+        ? <p>Drop the files here ...</p>
+        : <p>Drag 'n' drop some files here, or click to select files</p>}
+    </div>
+  );
 };
 
 export default FileUploader;
